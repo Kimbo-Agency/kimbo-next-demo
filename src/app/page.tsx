a9017ecefad810a982ea1ea8bf8beb061e2b22bd
Tim <tim@kimbodesign.ca>
2026-01-16 14:23:12 -0800

    removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { ApolloProvider } from '@apollo/client';
 import client from '../lib/apollo-client';
 import BannerImageSection from '../components/BannerImageSection';
 
-// Test
 
 
 export default function Page() {
